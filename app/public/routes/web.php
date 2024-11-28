eec64b10e6de42e2ad1e5b8a45a3ccf5d5699bc5
Orest <opokotylenkowork@gmail.com>
2024-11-28 01:54:23 +0100

    form and background added with no functionality

--- app/public/routes/web.php
+++ app/public/routes/web.php
@@ -3,3 +3,7 @@
 Route::add('/', function () {
     require(__DIR__ . "/../views/pages/index.php");
 });
+
+Route::add('/login', function () {
+    require(__DIR__ . "/../views/pages/login.php");
+});
--- app/public/views/partials/footer.php
+++ app/public/views/partials/footer.php
@@ -1,3 +1,6 @@
+<footer class="mt-auto" style="height: 200px; width: 100%; background-color:azure;">
+    <img src="/assets/images/bloodborne_logo.webp" alt="Bloodborne Logo" class="img-fluid" style="height: 60px;">
+</footer>
 </body>
 
 </html>
