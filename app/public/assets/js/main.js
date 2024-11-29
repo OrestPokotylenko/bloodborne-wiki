993b6494edd30998ad5f83b73eeb6c5e7be889cb
Orest <opokotylenkowork@gmail.com>
2024-11-29 01:40:28 +0100

    nav-bar added

--- app/public/assets/js/main.js
+++ app/public/assets/js/main.js
@@ -1,1 +1,31 @@
-console.log("hello from js!");
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname;
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        const href = link.getAttribute("href");
+        console.log(href);
+
+        if (href === currentPage) {
+            console.log("Matched");
+            link.classList.add("clicked");
+        }
+    });
+});
+
+if (isMobile) {
+    document.querySelectorAll('.dropdown-toggle').forEach(item => {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            const dropdownMenu = this.nextElementSibling;
+
+            if (dropdownMenu.style.display === 'block') {
+                dropdownMenu.style.display = 'none';
+            } else {
+                dropdownMenu.style.display = 'block';
+            }
+        });
+    });
+}
