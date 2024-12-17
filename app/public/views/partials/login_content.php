99c101e207869fea7863533c6abbd904e897c864
Orest <opokotylenkowork@gmail.com>
2024-12-17 13:10:27 +0100

    fixed not showing login form

--- app/public/views/partials/login_content.php
+++ app/public/views/partials/login_content.php
@@ -12,22 +12,21 @@
             </div>
             <div class="w-30 d-flex justify-content-between">
                 <button type="button">Forgot Password?</button>
-                <button type="button" id="signUpButton">Create New Account</button>
+                <button type="button" id="showSignUpButton">Create New Account</button>
             </div>
         </form>
         <div id="gSignIn" class="mt-4 w-100 d-flex justify-content-center"></div>
     </div>
-
     <div id="signUpForm" class="container position-absolute mt-5 mx-auto border rounded bg-dark top-50 start-50 translate-middle form-container w-auto d-none">
         <form class="d-flex flex-column justify-content-between align-items-center h-100" method="post" action="../../includes/signup.php">
             <div class="form-group w-75">
                 <input type="email" id="email" name="email" placeholder="Enter email">
             </div>
             <div class="form-group w-75">
-                <input type="text" id="username" name="username" placeholder="Enter username">
+                <input type="text" id="newUsername" name="username" placeholder="Enter username">
             </div>
             <div class="form-group w-75">
-                <input type="password" id="password" name="password" placeholder="Password">
+                <input type="password" id="newPassword" name="password" placeholder="Password">
             </div>
             <div class="form-group w-75">
                 <input type="password" id="repeatPassword" name="repeatPassword" placeholder="Repeat password">
@@ -37,7 +36,7 @@
             </div>
             <div class="w-30 d-flex justify-content-between">
                 <button type="button">Forgot Password?</button>
-                <button type="button" id="loginButton">Login</button>
+                <button type="button" id="showLoginButton">Login</button>
             </div>
         </form>
     </div>
